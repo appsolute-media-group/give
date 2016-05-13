74d1dd7459700a9685d7ebef49750b35d3e5a1c2
Kirk Walker <kirkwalker@Kirks-MacBook-Pro.local>
2016-05-13 14:27:10 -0700

    Added banner ad include file.

--- views/shop.php
+++ views/shop.php
@@ -464,7 +464,7 @@
     <div class="row ad-footer">
       <div class="ad-container">
         <div class="col-xs-12 ad-contents text-center">
-          <img src="images/spot.png" />
+          <?php include_once(ROOT_DIR.'/includes/banners.php'); ?>
         </div>
       </div>
     </div>
@@ -472,15 +472,6 @@
 </div>
 
 
-  <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-  <script>
-  (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='https://www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X');ga('send','pageview');
-    </script>
 
     <script src="scripts/vendor.js"></script>
 
@@ -488,5 +479,5 @@
 
     <script src="scripts/main.js"></script>
 
-    <script src="scripts/main.js"></script>
+
   </body>
