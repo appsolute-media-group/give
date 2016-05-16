43caba0be09cd4a62c1b24a8b18b747bc410bbc0
Daniel Juszko <daniel_juszko@hotmail.com>
2016-05-15 21:44:31 -0700

    fixed sign in error

--- views/login.php
+++ views/login.php
@@ -110,58 +110,6 @@
 
 
 
-  <!-- Redeem Code Modal -->
-  <div class="modal fade" id="referralCodeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content ">
-              
-
-        <div class="row" data-view="3">
-          <div class="col-xs-12 logo-container text-center">
-            <div class="logo-inner">
-                <img src="/images/Club-Appetite-Logo-white.png" alt="" class="logo-white"/>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 redeem-container text-center">
-            <div class="redeem-inner">
-                <p>
-                  Do you have a points code to redeem?
-                </p>
-                <p>
-                  Enter below to receive your Club Appetite points.
-                </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 input-container text-center">
-            <div class="input-inner">
-                <input type="text" name="signup_code" value="" placeholder="******">
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 button-container text-center">
-            <div class="button-inner">
-                <button type="button" name="sign_up" class="btn btn-redeem">Redeem</button>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-xs-12 button-container text-center">
-            <div class="button-inner">
-                <button type="button" name="sign_up" class="btn btn-skip" data-dismiss="modal">Skip</button>
-            </div>
-          </div>
-        </div>
-      </di v>
-
-    </div>
-  </div>
-
-
 
   <div class="view" data-view="4">
     <div class="view-container">
@@ -211,6 +159,63 @@
   </div>
 </div>
 </div>
+
+
+
+<!-- Redeem Code Modal -->
+  <div class="modal fade" id="referralCodeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content ">
+              
+
+        <div class="row" data-view="3">
+          <div class="col-xs-12 logo-container text-center">
+            <div class="logo-inner">
+                <img src="/images/Club-Appetite-Logo-white.png" alt="" class="logo-white"/>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 redeem-container text-center">
+            <div class="redeem-inner">
+                <p>
+                  Do you have a points code to redeem?
+                </p>
+                <p>
+                  Enter below to receive your Club Appetite points.
+                </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 input-container text-center">
+            <div class="input-inner">
+                <input type="text" name="signup_code" value="" placeholder="******">
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 button-container text-center">
+            <div class="button-inner">
+                <button type="button" name="sign_up" class="btn btn-redeem">Redeem</button>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-12 button-container text-center">
+            <div class="button-inner">
+                <button type="button" name="sign_up" class="btn btn-skip" data-dismiss="modal">Skip</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+
+
 </div>
 <script src="/scripts/vendor.js"></script>
 <script src="/scripts/plugins.js"></script>
