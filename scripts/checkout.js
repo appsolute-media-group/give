80af1ed0e38c35fd74ddd56670e9528718586d35
Kirk Walker <kirkwalker@Kirks-MacBook-Pro.local>
2016-05-18 11:54:46 -0700

    Missing main.js file include

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,21 +1,5 @@
 'use strict';
 
-function goToView(view) {
-  $('.view:visible').hide();
-  $('.view[data-view=' + '"' + view + '"' + ']').show();
-}
-
-function nextView() {
-  var current = parseInt($('.view:visible').attr('data-view'));
-  var next = current + 1;
-  goToView(next);
-}
-
-function prevView() {
-  var current = parseInt($('.view:visible').attr('data-view'));
-  var prev = current - 1;
-  goToView(prev);
-}
 
 $(document).ready(function () {
 
@@ -25,13 +9,3 @@ $(document).ready(function () {
 });
 //# sourceMappingURL=main.js.map
 
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
-}
--- views/directory_detail.php
+++ views/directory_detail.php
@@ -227,6 +227,7 @@
 
 <script src="/scripts/vendor.js"></script>
 <script src="/scripts/plugins.js"></script>
-<script src="/scripts/checkout.js"></script>
+
+<script src="/scripts/main.js"></script>
 
 </body>
